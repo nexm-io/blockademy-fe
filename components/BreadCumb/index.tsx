ab4c0c6be4a5f641755030fe3a33b1ba0f51ecc0
HuyTrnn <htran5655@gmail.com>
2023-08-29 16:18:25 +0700

    feat(article): ui

---
+++ components/BreadCumb/index.tsx
@@ -0,0 +1,39 @@
+const BreadCumb = () => {
+  return (
+    <>
+      <nav className="w-full rounded-md">
+        <ol className="list-reset flex text-gray-300 ">
+          <li className="leading-[23px]">
+            <a
+              href="#"
+              className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+            >
+              Home
+            </a>
+          </li>
+          <li className="leading-[23px]">
+            <span className="mx-2 text-neutral-500 dark:text-neutral-400">
+              {">"}
+            </span>
+          </li>
+          <li className="leading-[23px]">
+            <a
+              href="#"
+              className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+            >
+              Article
+            </a>
+          </li>
+          <li>
+            <span className="mx-2 text-neutral-500 dark:text-neutral-400">
+              {">"}
+            </span>
+          </li>
+          <li className="text-neutral-500 dark:text-neutral-400">Bitcoin Spot ETF vs. Bitcoin Futures ETF: What's the Difference?</li>
+        </ol>
+      </nav>
+    </>
+  );
+};
+
+export default BreadCumb;
