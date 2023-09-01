d6883730b17a20a5802b4c246f90e170d728b2d5
LAPTOP-AD6TEQ1R\Admin <ngotrieuhao@gmail.com>
2023-09-01 17:13:56 +0700

    fix(login): toast

--- app/(container)/layout.tsx
+++ app/(container)/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import PageContainer from "@/components/PageContainer";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
   title: {
@@ -18,13 +16,10 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        <PageContainer>{children}</PageContainer>
-        <Footer />
-        <ToastContainer />
-      </body>
-    </html>
+    <>
+      <Header />
+      <PageContainer>{children}</PageContainer>
+      <Footer />
+    </>
   );
 }
