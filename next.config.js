c90be70b6c3acc8bee14defd5ab816f53f15e243
yangiv <khoahua.yiv@gmail.com>
2023-09-15 20:27:10 +0700

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["admin-beta.blockademy.ai", "cdn-beta.blockademy.ai"],
+    remotePatterns: [
+      {
+        hostname: "**",
+        pathname: "**",
+        protocol: "https",
+      },
+    ],
   },
 };
 
