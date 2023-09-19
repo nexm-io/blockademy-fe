bb9b40e794f237f13fc10421c8215741b195f349
LAPTOP-AD6TEQ1R\Admin <ngotrieuhao@gmail.com>
2023-09-19 13:47:09 +0700

    feat(youtube): integrate youtube link

--- components/VideoPlayer/index.tsx
+++ components/VideoPlayer/index.tsx
@@ -1,14 +1,17 @@
 "use client";
+import { extractYouTubeVideoId } from "@/utils/getIdYoutube";
 import React, { useEffect, useRef, useState } from "react";
 import YouTube, { YouTubeProps } from "react-youtube";
 const VideoPlayer = ({
   onChangeForm,
   url,
   onChangeStatus,
+  typeUpload,
 }: {
   onChangeForm: Function;
   url: string;
   onChangeStatus: Function;
+  typeUpload: string;
 }) => {
   const handleEnd = () => {
     onChangeForm(true);
@@ -24,8 +27,8 @@ const VideoPlayer = ({
   };
 
   const opts: YouTubeProps["opts"] = {
-    height: "390",
-    width: "640",
+    height: "437",
+    width: "753",
     playerVars: {
       autoplay: 1,
     },
@@ -33,29 +36,33 @@ const VideoPlayer = ({
 
   return (
     <div className="w-full" onClick={handleStart}>
-      <video
-        className="w-full"
-        height="437"
-        controls
-        onEnded={handleEnd}
-        controlsList="nodownload"
-        autoPlay
-      >
-        <source
-          src={
-            url ||
-            "https://cdn-beta.blockademy.ai/videos/2023/09/1694422478meet-vzr-pxqc-vsq-and-3-more-pages-personal-microsoft-edge-2023-09-08-15-37-01mp425mp4"
-          }
-          type="video/mp4"
+      {typeUpload === "embed" && (
+        <YouTube
+          videoId={extractYouTubeVideoId(url) || undefined}
+          opts={opts}
+          onReady={onPlayerReady}
+          onEnd={handleEnd}
         />
-        Your browser does not support the video tag.
-      </video>
-      {/* <YouTube
-        videoId="GTw4fNvorZs"
-        opts={opts}
-        onReady={onPlayerReady}
-        onEnd={handleEnd}
-      /> */}
+      )}
+      {typeUpload === "manual" && (
+        <video
+          className="w-full"
+          height="437"
+          controls
+          onEnded={handleEnd}
+          controlsList="nodownload"
+          autoPlay
+        >
+          <source
+            src={
+              url ||
+              "https://cdn-beta.blockademy.ai/videos/2023/09/1694422478meet-vzr-pxqc-vsq-and-3-more-pages-personal-microsoft-edge-2023-09-08-15-37-01mp425mp4"
+            }
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      )}
     </div>
   );
 };
