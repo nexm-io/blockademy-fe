8673bc42370d2b0d3ab4b1580fda0c083fd15331
huynhthienba <ba.huynh@apps-cyclone.com>
2023-11-06 15:38:35 +0700

    fix: move back to top to component

---
+++ components/BackToTop/index.js
@@ -0,0 +1,46 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import IconBackToTop from "@/public/icons/backToTop.svg";
+import React from "react";
+
+const BackToTop = () => {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition =
+        window.scrollY || document.documentElement.scrollTop;
+      const pageHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrollTrigger = 0.6;
+      const shouldShowBackToTop = scrollPosition / pageHeight >= scrollTrigger;
+      setShowBackToTop(shouldShowBackToTop);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    showBackToTop && (
+      <button
+        className="fixed flex items-center justify-center bottom-[60px] right-[75px] animate-bounce w-[60px] h-[60px] rounded-lg bg-white-100 hover:brightness-90 shadow-3xl"
+        onClick={scrollToTop}
+      >
+        <Image alt="btn" src={IconBackToTop} width={40} height={40} />
+      </button>
+    )
+  );
+};
+
+export default BackToTop;
--- views/Home/MainPage/index.tsx
+++ views/Home/MainPage/index.tsx
@@ -2,9 +2,7 @@
 import TopBody from "@/views/Home/TopBody";
 import JourneyAcademy from "@/views/Home/JourneyAcademy";
 import SpecialContent from "@/views/Home/SpecialContent";
-import { useEffect, useState } from "react";
-import Image from 'next/image';
-import BackToTop from '@/public/icons/backToTop.svg'
+import BackToTop from "@/components/BackToTop";
 
 // TODO: remove component connection
 // import ListCard from "@/views/Home/ListCard";
@@ -18,44 +16,12 @@ import BackToTop from '@/public/icons/backToTop.svg'
 // import ListCardRecommend from "@/views/Home/ListCardRecommend";
 
 const MainPage = () => {
-  const [showBackToTop, setShowBackToTop] = useState(false);
-
-  useEffect(() => {
-    function handleScroll() {
-      const scrollPosition =
-        window.scrollY || document.documentElement.scrollTop;
-      const pageHeight =
-        document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
-      const scrollTrigger = 0.6;
-      const hideTrigger = 0.98;
-      const shouldShowBackToTop = scrollPosition / pageHeight >= scrollTrigger;
-      setShowBackToTop(shouldShowBackToTop);
-  
-    }
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
   return (
     <>
       <TopBody />
       <JourneyAcademy />
       <SpecialContent />
-      {showBackToTop && (
-        <button className="fixed flex items-center justify-center bottom-[60px] right-[75px] animate-bounce w-[60px] h-[60px] rounded-lg bg-white-100 hover:brightness-90 shadow-3xl" onClick={scrollToTop}>
-      
-        <Image alt="btn" src={BackToTop} width={40} height={40}/>
-      </button>
-      )}
+      <BackToTop />
     </>
   );
 };
