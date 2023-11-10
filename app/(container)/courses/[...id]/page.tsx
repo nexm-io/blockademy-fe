0b907d773cb49b4bab1b31440964c75c09e077fc
thienba <huynhthienba4@gmail.com>
2023-11-11 00:04:52 +0700

    feat(course-detail): update flow - ui

---
+++ app/(container)/courses/[...id]/page.tsx
@@ -0,0 +1,8 @@
+import CourseDetail from "@/views/Courses/CourseDetail";
+export default function CourseDetailPage() {
+  return (
+    <div className="mt-[74px]">
+      <CourseDetail />
+    </div>
+  );
+}
