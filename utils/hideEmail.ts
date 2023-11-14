50cf0af4ea3bc8e1b575ede0b54054c12519e5b5
thienba <huynhthienba4@gmail.com>
2023-11-14 21:18:48 +0700

    fix: util hide email

--- utils/hideEmail.ts
+++ utils/hideEmail.ts
@@ -1,13 +1,13 @@
 export function hideEmail(email: string): string {
-  if(email) {
+  if (email) {
     const atIndex = email.indexOf("@");
-  if (atIndex !== -1) {
-    const username = email.substring(0, atIndex);
-    const maskedUsername =
-      username.substring(0, 3) + "*".repeat(username.length - 3);
-    return maskedUsername + email.substring(atIndex);
-  }
-  return email;
+    if (atIndex !== -1) {
+      const username = email.substring(0, atIndex);
+      const maskedUsername =
+        username.length > 3 ? username.substring(0, 3) + "*".repeat(username.length - 3) : username;
+      return maskedUsername + email.substring(atIndex);
+    }
+    return email;
   } else {
     return " "
   }
