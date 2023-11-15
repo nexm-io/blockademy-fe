ecce45abf6f4999201c477dac3a7b2f6e507416c
huynhthienba <ba.huynh@apps-cyclone.com>
2023-11-15 14:18:19 +0700

    fix(my-rewards): update container layotu

--- app/(container)/my-rewards/page.tsx
+++ app/(container)/my-rewards/page.tsx
@@ -1,11 +1,12 @@
-import ListRewards from "@/views/Rewards/ListRewards";
 import React from "react";
 import ClaimReward from "./claimed-rewards/page";
 
 const MyRewardPage = () => {
-  return <div className="mt-[74px]">
-    <ClaimReward />
-  </div>;
+  return (
+    <div className="mt-[74px]">
+        <ClaimReward />
+    </div>
+  );
 };
 
 export default MyRewardPage;
