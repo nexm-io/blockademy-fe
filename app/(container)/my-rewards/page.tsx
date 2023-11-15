23991cf7b3c8548d6e52b3d567fae62b404e5477
Tra Tran <tra.tran@apps-cyclone.com>
2023-11-15 11:40:25 +0700

    fix(reward): show page reward

--- app/(container)/my-rewards/page.tsx
+++ app/(container)/my-rewards/page.tsx
@@ -1,9 +1,10 @@
 import ListRewards from "@/views/Rewards/ListRewards";
 import React from "react";
+import ClaimReward from "./claimed-rewards/page";
 
 const MyRewardPage = () => {
   return <div className="mt-[74px]">
-    <ListRewards />
+    <ClaimReward />
   </div>;
 };
 
