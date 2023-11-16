c00ffd0f37c224eacbb0643634ea98f9b0aac70c
huynhthienba <ba.huynh@apps-cyclone.com>
2023-11-16 10:59:46 +0700

    fix: dropdown

--- app/(container)/accomplishments/page.tsx
+++ app/(container)/accomplishments/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Accomplishments from "@/views/Rewards/ListRewards";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Accomplishments",
+};
 
 const AccomplishmentsPage = () => {
   return (
