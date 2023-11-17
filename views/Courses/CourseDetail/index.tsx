72d830f30e0f653dc1f09f097da870d3cdcf26bd
huynhthienba <ba.huynh@apps-cyclone.com>
2023-11-17 17:21:00 +0700

    fix(course-detail): update course desc style

--- views/Courses/CourseDetail/index.tsx
+++ views/Courses/CourseDetail/index.tsx
@@ -392,7 +392,7 @@ const CourseDetail = () => {
                             <div className="text-black-100 md:text-lg text-base font-normal mb-9">
                               <div
                                 id="content"
-                                className="flex flex-col gap-3 text-xs course-content md:text-base"
+                                className="flex flex-col gap-3 course-content text-base"
                                 dangerouslySetInnerHTML={{
                                   __html: lesson.lesson_description,
                                 }}
