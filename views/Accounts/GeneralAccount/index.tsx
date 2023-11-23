5102471c97b21f5261855c42be388b07e8041a4f
thienba <huynhthienba4@gmail.com>
2023-11-23 23:22:15 +0700

    feat(account-setting): responsive

--- views/Accounts/GeneralAccount/index.tsx
+++ views/Accounts/GeneralAccount/index.tsx
@@ -24,7 +24,7 @@ const GeneralAccount = () => {
 
   return (
     <div className="rounded-lg border border-grey-200 p-4 bg-gray-900">
-      <div className="flex items-center justify-between gap-2 mb-6">
+      <div className="hidden sm:flex items-center justify-between gap-2 mb-6">
         <h2 className="text-2xl">General Account</h2>
         <Button
           className="w-[106px] !py-[6px] bg-blue-600 group hover:bg-blue-600/50 group"
@@ -37,8 +37,8 @@ const GeneralAccount = () => {
       </div>
       {toggle ? (
         <div className="flex flex-col gap-6">
-          <div className="flex justify-between items-center md:flex-row flex-col">
-            <div className="flex gap-6 items-center">
+          <div className="flex justify-between sm:items-center md:flex-row flex-col">
+            <div className="flex gap-6 items-start sm:items-center">
               <div className="w-10 h-10 rounded-full bg-[#DDE8FF] flex justify-center items-center">
                 <Image
                   alt="avatar icon"
@@ -48,21 +48,26 @@ const GeneralAccount = () => {
                   className="select-none"
                 />
               </div>
-              <div className="flex flex-col ">
+              <div className="flex flex-col gap-2 sm:gap-[2px]">
                 <h3>Your name</h3>
                 <p className="font-light text-gray-300">
                   Set a customized name for your profile
                 </p>
+                <div className="flex md:hidden sm:block justify-start w-full sm:w-auto">
+                  <span className="text-blue-100">
+                    {userAccount?.first_name} {userAccount?.last_name}
+                  </span>
+                </div>
               </div>
             </div>
-            <div className=" my-4 md:my-0">
+            <div className="my-4 md:my-0 hidden md:flex sm:block justify-start w-full sm:w-auto">
               <span className="text-blue-100">
                 {userAccount?.first_name} {userAccount?.last_name}
               </span>
             </div>
           </div>
-          <div className="flex justify-between items-center md:flex-row flex-col">
-            <div className="flex gap-6 items-center">
+          <div className="flex justify-between items-start sm:items-center md:flex-row flex-col">
+            <div className="flex gap-6 items-start sm:items-center">
               <div className="w-10 h-10 rounded-full bg-[#DDE8FF] flex justify-center items-center">
                 <Image
                   alt="avatar icon"
@@ -72,21 +77,24 @@ const GeneralAccount = () => {
                   className="select-none"
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col gap-2 sm:gap-[2px]">
                 <h3>Email</h3>
                 <p className="font-light text-gray-300">
                   Edit your email
                 </p>
+                <div className="block md:hidden text-blue-100">
+                  {userAccount?.email}
+                </div>
               </div>
             </div>
-            <div className=" my-4 md:my-0">
+            <div className="hidden md:block my-4 md:my-0">
               <span className="text-blue-100">
                 {userAccount?.email}
               </span>
             </div>
           </div>
-          <div className="flex justify-between items-center md:flex-row flex-col">
-            <div className="flex gap-6 items-center">
+          <div className="flex justify-between items-start sm:items-center md:flex-row flex-col">
+            <div className="flex gap-6 items-start sm:items-center">
               <div className="w-10 h-10 rounded-full bg-[#DDE8FF] flex justify-center items-center">
                 <Image
                   alt="avatar icon"
@@ -96,18 +104,31 @@ const GeneralAccount = () => {
                   className="select-none"
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col gap-2 sm:gap-[2px]">
                 <h3>Phone</h3>
                 <p className="font-light text-gray-300">
                   Edit your phone number
                 </p>
+                <div className="block md:hidden text-blue-100">
+                  {userAccount?.phone}
+                </div>
               </div>
             </div>
-            <div className=" my-4 md:my-0">
+            <div className="hidden md:block my-4 md:my-0">
               <span className="text-blue-100">
                 {userAccount?.phone}
               </span>
             </div>
+          </div>
+          <div className="flex sm:hidden justify-end">
+            <Button
+              className="w-[106px] !py-[6px] bg-blue-600 group hover:bg-blue-600/50 group"
+              onClick={() => setToggle(!toggle)}
+            >
+              <span className="text-blue-700 group-hover:text-blue-700/80 transition-all">
+                Change
+              </span>
+            </Button>
           </div>
         </div>
       ) : (
