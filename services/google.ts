9d7b17eae25415d95c08ec88721a50d6d68d2ee5
huynhthienba <ba.huynh@apps-cyclone.com>
2023-11-28 16:31:06 +0700

    fix(login-with-google): provider google import & ts ignore

--- services/google.ts
+++ services/google.ts
@@ -1,4 +1,5 @@
-const { GoogleOAuthProvider } = require("google-oauth-gsi");
+// @ts-ignore
+import { GoogleOAuthProvider } from "google-oauth-gsi";
 
 export const googleProvider = new GoogleOAuthProvider({
   clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
