3aa6423b13c27d36fe4d633108d62c296b7dc12e
huynhthienba <ba.huynh@apps-cyclone.com>
2023-12-20 09:11:36 +0700

    fix(maintainance-page): update layout

--- app/(maintainance)/maintainance/page.tsx
+++ app/(maintainance)/maintainance/page.tsx
@@ -4,11 +4,15 @@ import Image from "next/image";
 
 const Maintainance = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 text-gray-100 px-2 mt-[44px]">
+    <div className="flex flex-col items-center justify-center gap-4 text-gray-100 px-2 h-[70vh]">
       <p className="text-[40px] font-medium text-black-100 text-center">
         BlockAdemy is Under Maintenance
       </p>
-      <Image className="-rotate-45 w-[250px] lg:w-[350px]" alt="logo" src={mantainnance} />
+      <Image
+        className="-rotate-45 w-[250px] lg:w-[350px]"
+        alt="logo"
+        src={mantainnance}
+      />
       <p className="text-2xl max-w-[850px] font-light text-center text-black-100">
         {`We're sorry for the inconvenience, but BlockAdemy is currently
         undergoing maintenance to bring you an even better learning experience`}
